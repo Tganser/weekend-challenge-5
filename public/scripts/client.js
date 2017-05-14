3e28b7de390f2f8cda80e908663e70f2dbaaddcb
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-14 18:38:06 -0500

    hitting omdb and getting results on dom

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,12 +7,21 @@ myApp.controller('OmdbController', function($http) {
  var vm = this;
 
  vm.searchResults = [];
+ vm.searchInput = "";
 
- $http({
-       method: 'GET',
-       url: 'http://www.omdbapi.com/?s=<user search string>'
-     }).then(function success(response) {
-     console.log('response from OMDB: ', response.data);
-    vm.movieData = response.data;
-   });
- });
+ vm.searchOMDB = function() {
+     console.log('in search OMDB ng-click');
+     console.log("search input =", vm.searchInput);
+
+   $http({
+         method: 'GET',
+         url: 'http://www.omdbapi.com/?s=' + vm.searchInput
+       }).then(function success(response) {
+       console.log('response from OMDB: ', response.data.Search);
+      vm.searchResults = response.data.Search;
+      // console.log(vm.movieData);
+      // console.log(response.movieData);
+    });//end then
+
+};
+});//end controller
