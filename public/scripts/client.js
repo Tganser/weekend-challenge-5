312e4695a13d421938300e079784c2e58ece795b
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-14 17:29:07 -0500

    planning and outlining

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,18 @@
 console.log("ng ready!");
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('OmdbController', function($http) {
+ // view model
+ var vm = this;
+
+ vm.searchResults = [];
+
+ $http({
+       method: 'GET',
+       url: 'http://www.omdbapi.com/?s=<user search string>'
+     }).then(function success(response) {
+     console.log('response from OMDB: ', response.data);
+    vm.movieData = response.data;
+   });
+ });
