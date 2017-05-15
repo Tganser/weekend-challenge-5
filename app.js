14ec221fbb85649e14d76cc6bc22cb103a25a4e7
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-14 22:18:33 -0500

    functionality working, removing not working

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require( 'express' );
 var app = express();
 var bodyParser = require( 'body-parser' );
 var path = require( 'path' );
-
+var mongoose = require('mongoose');
 
 // uses
 app.use( express.static( 'public' ) );
@@ -15,6 +15,56 @@ app.use( bodyParser.json() );
 app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'public/views/index.html' ) );
 });
+
+//createDB
+mongoose.connect('localhost:27017/favmovies');
+
+//creating schema
+var theSchema = mongoose.Schema({
+  title: String,
+  year: String,
+  // id: Number,
+  poster: String,
+});
+
+//make schema collection
+var movies = mongoose.model('movies', theSchema);
+
+
+app.post('/addfavorite', function(req, res){
+  // console.log('req.body.name:', req.body.name);
+  console.log("in add favorite routes server");
+  var newMovie = movies(req.body);
+  console.log("req.body ->" + req.body);
+  console.log("new Movie = "+newMovie);
+  newMovie.save().then(function(){
+    res.sendStatus(200);
+
+  });
+});
+
+app.get('/getmovies', function(req, res){
+  console.log('in get movies route server');
+  movies.find().then(function(data){
+    res.send(data);
+  });
+});
+
+app.delete('/deletefavorite', function(req, res){
+  // console.log('req.body.name:', req.body.name);
+  console.log("in delete favorite routes server");
+  // console.log(req.params);
+  // console.log(req.params.id);
+  // movies.remove( { _id: req.params.id}, function(err) {
+  //  if ( err ) {
+  //    res.send( 400 );
+  //  } //end Error
+  //  else {
+  //    res.send( 200 );
+  //  } // end no error
+  // });
+
+  });
 
 // globals
 var port = process.env.PORT || 3456;
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,6 +8,7 @@ myApp.controller('OmdbController', function($http) {
 
  vm.searchResults = [];
  vm.searchInput = "";
+ vm.favorites = [];
 
  vm.searchOMDB = function() {
      console.log('in search OMDB ng-click');
@@ -23,5 +24,64 @@ myApp.controller('OmdbController', function($http) {
       // console.log(response.movieData);
     });//end then
 
+};//end searchOMBD()
+
+vm.addtoFavs = function(id) {
+  console.log("in add to favs function!");
+  console.log("id: ", id);
+  console.log(id.Title);
+
+  movieToSendFavs = {
+    title : id.Title,
+    year : id.Year,
+    // _id: id.imdbID,
+    poster: id.Poster
+  };
+
+console.log("movie to send: ", movieToSendFavs);
+
+  $http({
+    method: 'POST',
+    url: '/addfavorite',
+    data: movieToSendFavs
+  }).then(function success (response){
+    console.log("response from server: ", response);
+    vm.getFavorites();
+  });
+  // console.log(this.id);
+};//end vm.addtoFavs
+
+vm.getFavorites = function(){
+  console.log("in get favorites function");
+  $http({
+    method: 'GET',
+    url: '/getmovies',
+  }).then(function success (response){
+    console.log("response from server in get favorites: ", response);
+    vm.favorites = response.data;
+    console.log("vm.favorites = ",vm.favorites);
+  });
 };
+
+vm.removeFav = function(foo){
+  console.log("in remove favorites function");
+  console.log(foo);
+//
+//   objectToSend = {
+//     id: foo._id
+//   };
+//
+//   console.log("Sending: ", objectToSend);
+//
+//   $http({
+//     method: 'DELETE',
+//     url: '/deletefavorite/'+foo.id
+//     // data: objectToSend,
+//   }).then(function success (response){
+//     console.log(response);
+//     console.log("movie removed");
+//     vm.getFavorites();
+//   });
+};
+
 });//end controller
